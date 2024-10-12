477c61fc28ac7065cdcbaf00c1d228542957d4b6
Izak Halseide <ihals@colostate.edu>
2024-10-12 10:18:51 -0600

    Make dumpfile_info script use a memory dump length

--- analysis/dumpfile_info.py
+++ analysis/dumpfile_info.py
@@ -18,18 +18,20 @@ def main():
         binary_format = (input("Enter 'true' if dump uses binary format: ").lower() == "true")
 
     with open(dump_filename, 'rb') as file_in:
-        num_words = serial_analysis.file_seek_next_data_dump_and_count_it(file_in, binary_format)
+        num_words = 2**18
+        serial_analysis.file_seek_next_data_dump(file_in, binary_format)
         print(f"* data word count = {num_words}")
 
         hw = 0
         for _ in range(num_words):
+            #print(file_in.tell())
             hw += serial_analysis.hamming_weight(serial_analysis.file_read_next_hex4(file_in, binary_format))
         hw_percent = percent(hw, num_words*16)
         print(f"* Hamming weight = {hw} = {hw_percent}%")
 
         # Go to beginning of data dump again and find the min, max, and mode word value
         file_in.seek(0)
-        serial_analysis.file_seek_next_data_dump_and_count_it(file_in, binary_format)
+        serial_analysis.file_seek_next_data_dump(file_in, binary_format)
         all_data = serial_analysis.file_read_hex4_dump_as_words(file_in, num_words, binary_format)
         print(f"* First word = 0x{all_data[0]:04X}")
         print(f"* Last word = 0x{all_data[-1]:04X}")

130c9a0ee171464769bb191aec0773777774431c
Izak Halseide <ihals@colostate.edu>
2024-10-15 21:33:27 -0600

    Improve the Python script for dump folders

--- analysis/dump_dir_everything.py
+++ analysis/dump_dir_everything.py
@@ -1,6 +1,7 @@
 import os, sys, matplotlib
 import numpy as np
 import matplotlib.pyplot as plt
+
 from serial_analysis import *
 from find_bit_strength import combine_captures_as_votes
 from convert_dump import convert as binary_dump_to_text
@@ -34,18 +35,23 @@ def run(input_dir: str, output_dir: str) -> bool:
     # Create empty output directory
     os.mkdir(output_dir)
 
-    num_captures = 11
+    # num_captures = 11
+    num_captures = 49
     num_words = NUM_WORDS
 
-    for i, in_fname in enumerate(os.listdir(input_dir)):
+    listing = list(os.listdir(input_dir))
+    for i, in_fname in enumerate(listing):
         # Log
         print(f"({i + 1}): Processing \"{in_fname}\"")
+
+        # Get input filename before the extension to use to automatically create the output folder name
+        in_fname_no_ext, _ = in_fname.rsplit('.', maxsplit=1)
 
         # Construct the string for the full input file path
         in_path = os.path.join(input_dir, in_fname)
 
         # Create a new folder to hold all of the results for processing this file
-        out_path = os.path.join(output_dir, in_fname)
+        out_path = os.path.join(output_dir, in_fname_no_ext)
         print(f"Will save output in directory \"{out_path}\"")
         os.mkdir(out_path)
 
@@ -54,23 +60,43 @@ def run(input_dir: str, output_dir: str) -> bool:
         text_dump_path = os.path.join(out_path, "Hex-dumps.txt")
         binary_dump_to_text(in_path, text_dump_path, num_words)
 
+        # Create file with list of Hamming Weights for each dump...
+        # Get each dump's Hamming weight.
+        print("Reading Hamming weights for each dump")
+        hweights = []
+        with open(text_dump_path, "rb") as hex_dump_in:
+            for d_num in range(num_captures):
+                file_seek_next_data_dump(hex_dump_in)
+                hw = file_hamming_weight(hex_dump_in, num_words)
+                hweights.append(hw)
+        # Save Hamming weights to a new file
+        print("Saving each Hamming weight")
+        hweights_file = os.path.join(out_path, "Hamming-weights.txt")
+        with open(hweights_file, "w") as hweights_file_out:
+            for hw in hweights:
+                print(hw, percent(hw, num_words * BITS_PER_WORD), file=hweights_file_out)
+
         # Create PUF file
         print("Creating gold PUF")
         gold_puf_fname = os.path.join(out_path, "Gold-PUF.txt")
-        create_gold_puf_v2(num_captures, text_dump_path, gold_puf_fname, num_words)
+        gold_puf_num_captures = num_captures
+        if gold_puf_num_captures % 2 == 0:
+            # Force the number of captures for the gold PUF to be odd (by excluding the last one)
+            gold_puf_num_captures -= 1
+        create_gold_puf_v2(gold_puf_num_captures, text_dump_path, gold_puf_fname, num_words)
 
-        # Get Hamming Weight
-        hweight = file_hamming_weight(gold_puf_fname, num_words)
-        hweight_p = percent(hweight, num_words * BITS_PER_WORD)
+        # Get gold PUF Hamming Weight
+        puf_hweight = file_hamming_weight(gold_puf_fname, num_words)
+        puf_hweight_p = percent(puf_hweight, num_words * BITS_PER_WORD)
 
-        # Create gold PUF salt-and-pepper sample image
+        # Create salt-and-pepper sample image from first few bits of the gold PUF 
         for size in (64, 128, 256, 512):
             print(f"Creating {size}x{size} salt-and-pepper image")
             img_data = file_read_image(gold_puf_fname, size, size)
             f = plt.figure()
             ax = f.gca()
             ax.set_title(f"Gold PUF {size}x{size}")
-            ax.set_xlabel(f"HW = {hweight_p:.3f}%")
+            ax.set_xlabel(f"HW = {puf_hweight_p:.3f}%")
             show_binary_image_from_data(ax, img_data)
             f.savefig(os.path.join(out_path, f"Salt-and-pepper-{size}x{size}.png"))
 
@@ -80,8 +106,8 @@ def run(input_dir: str, output_dir: str) -> bool:
                 # Try different amounts of max votes, but only as many as are in the actual data
                 break
 
-            print("Creating bit stability/strengh vote histogram figures")
-            binary_votes = combine_captures_as_votes(num_captures, text_dump_path, num_words)
+            print(f"Creating bit stability/strengh vote histogram figures ({num_votes} votes)")
+            binary_votes = combine_captures_as_votes(num_votes, text_dump_path, num_words)
             
             max_votes_num = np.max(binary_votes)
 
